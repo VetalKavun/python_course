b9623a617170f401ce16df7e40bd1ea1c400ebb7
VITALIY KAVUN <kavun.v@dbbest.com>
2020-12-14 12:59:46 +0200

    added example of working with lists

--- section_2/basics.py
+++ section_2/basics.py
@@ -1,5 +1,18 @@
+#data types example
 x = 10
 y = "hello"
 z = 10.1
 
 print(type(x), type(y), type(z))
+#----------------------------------
+#lists example
+list_of_numbers = list(range(1, 10))
+print(list_of_numbers)
+list_of_numbers2 = list(range(1, 10, 2))
+print(list_of_numbers2)
+list_of_numbers3 = [2, 4, 6, 89]
+print(list_of_numbers3)
+list_of_strings = ['hi', 123]
+print(list_of_strings)
+list_of_combined_data = ['this', 123, 'is', 2354, 'possible']
+print(list_of_combined_data)
