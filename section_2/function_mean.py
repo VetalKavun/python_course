06b0a6b46dc5ccdbe449effbc675aa6ae1714070
VITALIY KAVUN <kavun.v@dbbest.com>
2020-12-15 17:21:02 +0200

    added dictionary support

--- section_2/function_mean.py
+++ section_2/function_mean.py
@@ -1,5 +1,11 @@
 def mean(mylist):
+    if type(mylist) == dict:
+        return sum(mylist.values()) / len(mylist)
+
     return sum(mylist) / len(mylist)
 
 test_list = [1, 2, 3, 4, 5]
 print(mean(test_list))
+
+test_list_dict = {"1": 1, "2": 2, "3": 3}
+print(mean(test_list_dict))
