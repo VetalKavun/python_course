bf0c83567bbd7ee750db703729b8b22a85cb8e0d
VITALIY KAVUN <kavun.v@dbbest.com>
2020-12-16 16:35:36 +0200

    example of string formatting

--- section_2/string_formatting.py
+++ section_2/string_formatting.py
@@ -2,5 +2,9 @@ def greeting(name):
     return "Hello, %s" % name
 
 
+def greeting2(name):
+    return f"Hello, {name}"
+
+
 name = input("Enter your name: ")
-print(greeting(name))
+print(greeting2(name))
