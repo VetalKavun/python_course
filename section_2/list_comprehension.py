b61aae0fdf5cdbadfaddc7f0e6e3a25c3930d1d2
VITALIY KAVUN <kavun.v@dbbest.com>
2020-12-18 14:19:05 +0200

    added simple list comprehension example. 65

--- section_2/list_comprehension.py
+++ section_2/list_comprehension.py
@@ -1,5 +1,5 @@
-temps = [221, 223, 225, 220]
+temps = [221, 223, 225, 220, -9999, 234]
 
-new_temp = [temp / 10 for temp in temps]
+new_temp = [temp / 10 for temp in temps if temp != -9999]
 
 print(new_temp)
