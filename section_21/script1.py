40487ca9af70f6c7a809c1d8ffecff7df8893964
VITALIY KAVUN <kavun.v@dbbest.com>
2021-02-22 17:06:08 +0200

    added delete and update functions

--- section_21/script1.py
+++ section_21/script1.py
@@ -24,8 +24,24 @@ def view():
     conn.close()
     return rows
 
-insert("book1", 14, 12.4)
+def delete(item):
+	conn = sqlite3.connect("lite.db")
+	cursor = conn.cursor()
+	cursor.execute("DELETE FROM store WHERE item = ?", (item,))
+	conn.commit()
+	conn.close()
+
+
+def update(item, quantity, price):
+	conn = sqlite3.connect("lite.db")
+	cursor = conn.cursor()
+	cursor.execute("UPDATE store SET quantity = ?, price = ? WHERE item = ?", (quantity, price, item))
+	conn.commit()
+	conn.close()
+
+# insert("book1", 14, 12.4)
 insert("book2", 34, 45.3)
 insert("book3", 23, 34.5)
+update("book2", 45, 49.3)
 print(view())
 
